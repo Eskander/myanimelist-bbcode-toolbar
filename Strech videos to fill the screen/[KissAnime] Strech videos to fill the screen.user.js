560bbd7f18cdf3ec1993dad9bcbf6fc204824617
Eskander Bejaoui <mail@eskander.tn>
2018-06-10 15:14:42 +0100

    Update [KissAnime] Strech videos to fill the screen.user.js

--- Strech videos to fill the screen/[KissAnime] Strech videos to fill the screen.user.js
+++ Strech videos to fill the screen/[KissAnime] Strech videos to fill the screen.user.js
@@ -1,13 +1,20 @@
 // ==UserScript==
 // @name         KissAnime Strech videos to fill the screen
 // @namespace    kissanime.ru
-// @version      0.1
+// @version      0.2
 // @description  Watch old animes prior to 2010 in 16:9 aspect ratio.
 // @author       Skqnder
 // @match        *://kissanime.ru/Anime/*/*
+// @compatible   firefox
+// @compatible   chrome
 // @grant        none
 // ==/UserScript==
 
-$(document).ready(function(){
-    $('#my_video_1_html5_api').css('object-fit', 'fill');
+$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e)    
+{
+    if (!window.screenTop && !window.screenY) {
+           $('#my_video_1_html5_api').css('object-fit', 'fill');
+    } else {
+           $('#my_video_1_html5_api').css('object-fit', 'contain');
+    }
 });
