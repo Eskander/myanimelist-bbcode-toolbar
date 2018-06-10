efc4a8bb2264049473050cda7d0a09013b65c838
Eskander Bejaoui <11521400+Skqnder@users.noreply.github.com>
2018-06-10 15:17:54 +0100

    Update [KissAnime] Strech videos to fill the screen.user.js

--- Strech videos to fill the screen/[KissAnime] Strech videos to fill the screen.user.js
+++ Strech videos to fill the screen/[KissAnime] Strech videos to fill the screen.user.js
@@ -1,20 +1,29 @@
 // ==UserScript==
-// @name         KissAnime Strech videos to fill the screen
-// @namespace    kissanime.ru
-// @version      0.2
-// @description  Watch old animes prior to 2010 in 16:9 aspect ratio.
+// @name         [KissAnime] Strech videos to fill the screen
+// @namespace    https://greasyfork.org/en/users/152412
+// @version      0.8
+// @description  Watch old anime in full screen 16:9 aspect ratio.
 // @author       Skqnder
+// @license 	 MIT
 // @match        *://kissanime.ru/Anime/*/*
-// @compatible   firefox
-// @compatible   chrome
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @compatible   firefox Tested with Tampermonkey
+// @compatible   chrome Tested with Tampermonkey
+// @compatible   opera Tested with Tampermonkey Beta
 // @grant        none
 // ==/UserScript==
 
-$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e)    
-{
-    if (!window.screenTop && !window.screenY) {
-           $('#my_video_1_html5_api').css('object-fit', 'fill');
+$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function (e) {
+
+    var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+
+    if (state == true) {
+        
+        $('#my_video_1_html5_api').css('object-fit', 'fill');
+        
     } else {
-           $('#my_video_1_html5_api').css('object-fit', 'contain');
+        
+        $('#my_video_1_html5_api').css('object-fit', 'contain');
+        
     }
 });
