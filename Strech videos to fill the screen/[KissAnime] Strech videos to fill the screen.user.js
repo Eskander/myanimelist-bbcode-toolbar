e49a9e37c60bf23495ac6db4083f0bdfd0993b91
Eskander Bejaoui <11521400+Skqnder@users.noreply.github.com>
2018-06-10 15:21:20 +0100

    Update [KissAnime] Strech videos to fill the screen.user.js

--- Strech videos to fill the screen/[KissAnime] Strech videos to fill the screen.user.js
+++ Strech videos to fill the screen/[KissAnime] Strech videos to fill the screen.user.js
@@ -1,39 +1,58 @@
-
 // ==UserScript==
 // @name         [KissAnime] Strech videos to fill the screen
-// @namespace    https://greasyfork.org/en/users/152412
-// @version      0.9
-// @description  Watch old anime in full screen 16:9 aspect ratio.
+// @namespace    https://greasyfork.org/users/152412
+// @version      0.11
+// @description  Watch old anime in full screen 16:9 aspect ratio. (all servers)
 // @author       Skqnder
 // @license 	 MIT
 // @match        *://kissanime.ru/Anime/*/*
 // @match        *://openload.co/embed/*/*
 // @match        *://www.rapidvideo.com/e/*
 // @match        *://streamango.com/embed/*/*
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @compatible   firefox Tested with Tampermonkey
 // @compatible   chrome Tested with Tampermonkey
 // @compatible   opera Tested with Tampermonkey Beta
 // @grant        none
 // ==/UserScript==
 
-$(document).on('webkitfullscreenchange mozfullscreenchange fullscreenchange', function (e) {
+document.addEventListener('webkitfullscreenchange', function(e) {
+    check();
+}, false);
 
+document.addEventListener('mozfullscreenchange', function(e) {
+    check();
+}, false);
+
+document.addEventListener('fullscreenchange', function(e) {
+    check();
+}, false);
+
+
+function check () {
     var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
-
     if (state === true) {
-
-        $('#olvideo_html5_api').css('object-fit', 'fill');
-        $('#videojs_html5_api').css('object-fit', 'fill');
-        $('#mgvideo_html5_api').css('object-fit', 'fill');
-        $('#my_video_1_html5_api').css('object-fit', 'fill');
-
+        fitvideo('olvideo_html5_api');
+        fitvideo('videojs_html5_api');
+        fitvideo('mgvideo_html5_api');
+        fitvideo('my_video_1_html5_api');
     } else {
-
-        $('#olvideo_html5_api').css('object-fit', 'contain');
-        $('#videojs_html5_api').css('object-fit', 'contain');
-        $('#mgvideo_html5_api').css('object-fit', 'contain');
-        $('#my_video_1_html5_api').css('object-fit', 'contain');
-
+        containvideo('olvideo_html5_api');
+        containvideo('videojs_html5_api');
+        containvideo('mgvideo_html5_api');
+        containvideo('my_video_1_html5_api');
     }
-});
+}
+
+function fitvideo(a) {
+  var x = document.getElementById(a);
+  if(typeof x !== 'undefined' && x !== null) {
+    x.style.cssText = 'object-fit: fill';
+  }
+}
+
+function containvideo(a) {
+  var x = document.getElementById(a);
+  if(typeof x !== 'undefined' && x !== null) {
+    x.style.cssText = 'object-fit: fit';
+  }
+}
