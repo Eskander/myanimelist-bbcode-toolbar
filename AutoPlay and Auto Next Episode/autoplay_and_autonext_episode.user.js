17a5560e0d031b7c9b09059630c8a0cec146bcbf
Eskander Bejaoui <mail@eskander.tn>
2018-06-10 15:53:27 +0100

    major rewrite for better performance

--- AutoPlay and Auto Next Episode/autoplay_and_autonext_episode.user.js
+++ AutoPlay and Auto Next Episode/autoplay_and_autonext_episode.user.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         [KissAnime] AutoPlay & Auto Next Episode
-// @namespace    https://greasyfork.org/en/users/152412
-// @version      0.9
-// @description  AutoPlay & Automatically move to the next episode (all servers).
+// @namespace    https://greasyfork.org/users/152412
+// @version      0.14
+// @description  AutoPlay & Automatically move to the next episode. (all servers)
 // @author       Skqnder
 // @license 	 MIT
 // @match        *://kissanime.ru/Anime/*/*
 // @match        *://openload.co/embed/*/*
 // @match        *://www.rapidvideo.com/e/*
 // @match        *://streamango.com/embed/*/*
-// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js
 // @run-at       document-start
 // @compatible   firefox Tested with Tampermonkey
 // @compatible   chrome Tested with Tampermonkey
@@ -18,106 +18,159 @@
 // @grant        GM_setValue
 // ==/UserScript==
 
-(function() {
+'use strict';
 
-    'use strict';
+(function () {
 
-    var observer = new MutationObserver(function(mutations) {
-        mutations.forEach(function(mutation) {
-            var nodes = mutation.addedNodes;
-            for (var i = 0; i < nodes.length; i++) {
-                if (nodes[i].nodeName == "VIDEO") {
-                    nodes[i].setAttribute('preload', 'none');
-                    nodes[i].removeAttribute('autoplay');
-                }
-            }
-        });
-    });
-
-    observer.observe(document.documentElement, {
+    var config = {
         childList: true,
         subtree: true
-    });
+    }
+    var observer = new MutationObserver(function (mutations) {
+        mutations.forEach(function (mutation) {
+            disableAutoplay(mutation);
+        })
+    })
 
-})();
+    function disableAutoplay(mutation) {
+        var nodes = mutation.addedNodes;
+        for (var i = 0; i < nodes.length; i++) {
+            if (nodes[i].nodeName == 'VIDEO') {
+                nodes[i].setAttribute('preload', 'none');
+                nodes[i].removeAttribute('autoplay');
+                //console.log('[Kiss] Video killed successfully.');
+            }
+        }
+    }
+
+    observer.observe(document.documentElement, config);
+
+})()
 
 $(document).ready(function () {
 
-    'use strict';
+    var AutPlay = GM_getValue('AutPlay', true);
+    var NxtPlay = GM_getValue('NxtPlay', true);
+    GM_setValue('EpEnded', false);
+    var timer = null;
+    var box1 = '';
+    var box2 = '';
+    //console.log("[Kiss]", window.location.host, "is ready");
 
-    var NxtPlay = GM_getValue('NxtPlay', 1);
-    var AutPlay = GM_getValue('AutPlay', 1);
-    var check1 = '';
-    var check2 = '';
-    GM_setValue('EpEnded', 0);
+    if (window.location.host == "kissanime.ru") {
 
-    if (NxtPlay === 1) {
-        check1 = 'checked';
-    } else {
-        check1 = '';
-    }
-
-    if (AutPlay === 1) {
-        check2 = 'checked';
-    } else {
-        check2 = '';
-    }
-
-    $('.barContent').after('<input type="checkbox" id="NxtPlay" '+check1+'> <label for="NxtPlay">Auto Next Episode</label> <br>');
-    $('.barContent').after('<br><input type="checkbox" id="AutPlay" '+check2+'> <label for="AutPlay">Auto Play</label> <br>');
-
-    $('#NxtPlay').change(function () {
-        if ($('#NxtPlay').is(":checked")) {
-            NxtPlay = 1;
-            GM_setValue('NxtPlay', 1);
+        if (AutPlay) {
+            box1 = 'checked';
         } else {
-            NxtPlay = 0;
-            GM_setValue('NxtPlay', 0);
+            box1 = '';
         }
-    });
 
-    $('#AutPlay').change(function () {
-        if ($('#AutPlay').is(":checked")) {
-            AutPlay = 1;
-            GM_setValue('AutPlay', 1);
+        if (NxtPlay) {
+            box2 = 'checked';
         } else {
-            AutPlay = 0;
-            GM_setValue('AutPlay', 0);
+            box2 = '';
         }
-    });
 
-    $('#olvideo_html5_api').on('ended', function (e) {
-        GM_setValue('EpEnded', 1);
-    });
-    $('#videojs_html5_api').on('ended', function (e) {
-        GM_setValue('EpEnded', 1);
-    });
-    $('#mgvideo_html5_api').on('ended', function (e) {
-        GM_setValue('EpEnded', 1);
-    });
-    $('#my_video_1_html5_api').on('ended', function (e) {
-        GM_setValue('EpEnded', 1);
-    });
+        var checkbox1 = '<input type="checkbox" id="AutPlay" ' + box1 + '><label for="AutPlay" title="Automatically play the video when the page loads">Auto Play</label>';
+        var checkbox2 = '<input type="checkbox" id="NxtPlay" ' + box2 + '><label for="NxtPlay" title="Automatically move to the next episode at the end of the current one">Auto Next Episode</label>';
 
-    setInterval(function(){
-        var EpEnded = GM_getValue('EpEnded', 0);
-        if( EpEnded === 1 ) {
-            Nxt();
-            GM_setValue('EpEnded', 0);
+        $('.barContent').append('<br>' + checkbox1);
+        $('.barContent').append('<br>' + checkbox2);
+
+        $('#AutPlay').change(function () {
+            if ($('#AutPlay').is(':checked')) {
+                AutPlay = true;
+                GM_setValue('AutPlay', true);
+            } else {
+                AutPlay = false;
+                GM_setValue('AutPlay', false);
+            }
+            //console.log("[Kiss] Auto Next is", AutPlay);
+        });
+
+        $('#NxtPlay').change(function () {
+            if ($('#NxtPlay').is(':checked')) {
+                NxtPlay = true;
+                GM_setValue('NxtPlay', true);
+            } else {
+                NxtPlay = false;
+                GM_setValue('NxtPlay', false);
+            }
+            //console.log("[Kiss] Auto Next is", NxtPlay);
+        });
+
+        if (!$('#my_video_1_html5_api').length) {
+            setInterval(function () {
+                var EpEnded = GM_getValue('EpEnded', false);
+                //console.log("1 sec passed.");
+                if (EpEnded) {
+                    GM_setValue('EpEnded', 0);
+                    if (NxtPlay) {
+                        $('#btnNext').click();
+                        //console.log("[Kiss] Requesting next episode..");
+                    }
+                }
+            }, 2000); /* embedded videos state check interval. */
         }
-    }, 1000); /* interval of video state check. */
 
-    function Nxt() {
-        if (NxtPlay === 1) {
-            $('#btnNext').click();
+        /* Just a stupid hack to please the guys at Google's */
+        if ($("#divContentVideo > iframe").length && AutPlay && typeof InstallTrigger == 'undefined') {
+            $("#divContentVideo > iframe")[0].setAttribute("allow", "autoplay; fullscreen");
+            //console.log("[Kiss] Stupid chrome detected");
+            setTimeout(function () {
+                $("#divContentVideo > iframe")[0].src += '';
+            }, 1000); /* autoplay delay in chrome. */
         }
+
     }
 
-    window.onload = function() {
-        if (AutPlay === 1) {
-            $("#videooverlay").click();
+    if (window.location.host == "kissanime.ru" && $('#my_video_1_html5_api').length) {
+
+        if (AutPlay) {
+            $('.videoAdClose').remove();
             $('.vjs-big-play-button').click();
         }
-    };
 
-})();
+        $('#my_video_1_html5_api').on('ended', function (e) {
+            if (NxtPlay) {
+                //console.log("[Kiss] Requesting next episode..");
+                $('#btnNext').click();
+            }
+        });
+
+    } else if (window.location.host == "www.rapidvideo.com") {
+
+        if (AutPlay) {
+            $('.vjs-poster').remove();
+            $('.vjs-text-track-display').remove();
+            $('.vjs-big-play-button').click();
+        }
+
+        $('#videojs_html5_api').on('ended', function (e) {
+            GM_setValue('EpEnded', true);
+        });
+
+    } else if (window.location.host == "openload.co") {
+
+        if (AutPlay) {
+            $('#videooverlay').click();
+            $('.vjs-poster').click();
+        }
+
+        $('#olvideo_html5_api').on('ended', function (e) {
+            GM_setValue('EpEnded', true);
+        });
+
+    } else if (window.location.host == "streamango.com") {
+
+        if (AutPlay) {
+            $('#videooverlay').click();
+            $('.vjs-big-play-button').click();
+        }
+
+        $('#mgvideo_html5_api').on('ended', function (e) {
+            GM_setValue('EpEnded', true);
+        });
+    }
+
+});
