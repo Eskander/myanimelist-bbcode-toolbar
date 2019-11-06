515769220671eaff0599570e13862a10128db4db
Eskander Bejaoui <11521400+Eskander@users.noreply.github.com>
2019-11-06 22:44:09 +0100

    Update complete_captcha_removal.user.js

--- KissAnime Complete captcha removal/complete_captcha_removal.user.js
+++ KissAnime Complete captcha removal/complete_captcha_removal.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         [KissAnime] Complete captcha removal
 // @namespace    https://github.com/eskander
-// @version      0.3.1
+// @version      0.4
 // @description  Always use HydraX and never encounter captchas again.
 // @author       eskander
 // @license      MIT
