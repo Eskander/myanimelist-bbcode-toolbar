3649ea7f28bc5ef59cdf228f778054015f15cb53
Eskander Bejaoui <mail@eskander.tn>
2021-11-20 07:32:00 +0100

    Bump version number

--- myanimelist_bbcode_toolbar.user.js
+++ myanimelist_bbcode_toolbar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         MyAnimeList BBCode Toolbar
 // @namespace    https://github.com/eskander
-// @version      2021-01-01
+// @version      2021-11-20
 // @description  Advanced BBCode Editor for MyAnimeList.net
 // @author       eskander
 // @license      MIT
